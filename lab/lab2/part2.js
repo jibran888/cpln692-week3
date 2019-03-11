e91fd1d7956829c8479c5d9c8192e4108fc8d1e9
Jibran888 <jibran.n.khan@gmail.com>
2019-03-11 14:04:17 -0400

    HW 3 - updated/completed.

--- lab/lab2/part2.js
+++ lab/lab2/part2.js
@@ -18,3 +18,42 @@ _.each(myArray, function(value, key, list) {
   console.log(value, key, list);
 });
 ===================== */
+
+//replacing first for loop
+
+var stationName = (arr) => {for (var i = 1; i < bikeArrayClean.length; i = i+1){
+  console.log(arr[i][2])}}
+
+var namingStations = (arr) => console.log(arr[i][2])
+
+
+
+
+// replacing second for loops
+
+var theArray = ['A short story.', 42, 55, ['Another story'], 23, 12, 2, 4, 5, 6, 'bar', 'foo'];
+
+// Here's the variable we need to hold our count in:
+var exampleSum = 0; // To count of the strings
+
+// A loop for summing the contents of theArray
+for (var i = 0; i < theArray.length; i++) {
+  var arrayValue = theArray[i];
+  if (typeof arrayValue === 'string') {         // String case
+    exampleSum = exampleSum + arrayValue.length
+  } else if (typeof arrayValue === 'number') {  // Number case
+    exampleSum = exampleSum + arrayValue
+  } else {                                      // Otherwise
+    console.log("Not sure how to proceed with value:", arrayValue)
+  }
+}
+
+var 
+
+
+
+  testArr = [["ross", "bernet"], ["Jibran", "khan"]]
+
+  var logNames = (arr) => {console.log(arr[0])}
+
+  _.each(testArr, logNames)
